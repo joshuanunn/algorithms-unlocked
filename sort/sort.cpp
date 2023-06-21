448dc6d277e8185c37e7ad5c286d5627ca305d2d
Joshua Nunn <joshuanunn@hotmail.co.uk>
2023-06-21 09:20:18 +0100

    Add mergesort implementation

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <cstdlib>
 #include <iostream>
+#include <climits>
 #include <numeric>
 #include <vector>
 
@@ -104,6 +105,69 @@ void insertion_sort(std::vector<int> &A, int n) {
 }
 
 /*
+ * @brief Supporting function for merge sort implementation
+ *
+ * @param[in]  A  reference to vector of ints, where subarrays
+ *                A[p..q] and A[q+1..r] are assumed sorted
+ * @param[in]  p  index of subarrays of A (A[p..q] and A[q+1..r])
+ * @param[in]  q  index of subarrays of A (A[p..q] and A[q+1..r])
+ * @param[in]  r  index of subarrays of A (A[p..q] and A[q+1..r])
+ * */
+void merge(std::vector<int> &A, int p, int q, int r) {
+    // Calculate sizes of each subarray A[p..q] and A[q+1..r]
+    int n1 = q - p + 1;
+    int n2 = r - q;
+
+    // Create a copy of subarray B = A[p..q], terminated with MAX_INT
+    std::vector<int> B(n1+1); // Allocate extra element for MAX_INT
+    for(int i=p; i<=q; i++) {
+        B[i - p] = A[i];
+    }
+    B[n1] = INT_MAX;
+
+    // Create a copy of subarray C = A[q+1..r], terminated with MAX_INT
+    std::vector<int> C(n2+1); // Allocate extra element for MAX_INT
+    for(int i=q+1; i<=r; i++) {
+        C[i - (q + 1)] = A[i];
+    }
+    C[n2] = INT_MAX;
+
+    // Merge and sort subarrays B and C into A[p..r]
+    int i=0;
+    int j=0;
+
+    for(int k=p; k<=r; k++) {
+        if (B[i] <= C[j]) {
+            A[k] = B[i++];
+        } else {
+            A[k] = C[j++];
+        }
+    }
+}
+
+/*
+ * @brief Merge sort implementation
+ *
+ * @param[in]  A  reference to vector of ints to be sorted
+ * @param[in]  p  start index of subarray of A
+ * @param[in]  r  end index of subarray of A
+ * */
+void merge_sort(std::vector<int> &A, int p, int r) {
+    // Trivial basecase for one or zero elements, as they are "sorted"
+    if (p >= r) return;
+
+    // Calculate midpoint of array range
+    int q = (p + r) / 2;
+
+    // Recursively sort each subarray A[p..q] and A[q+1..r]
+    merge_sort(A, p, q);
+    merge_sort(A, q+1, r);
+
+    // Finally, merge sorted subarrays
+    merge(A, p, q, r);
+}
+
+/*
  * @brief Verify elements of a vector of ints are monotonically increasing
  *
  * @param[in]  A  reference to vector of ints to be checked
@@ -190,6 +254,34 @@ int main(int argc, char* argv[]) {
     }
 
     std::cout << "Insertion sort: " << ((float)dt / (1e6 * repeats)) << " s (average per op)" <<std::endl;
+
+
+    // Merge sort
+    dt = 0;
+    for (int i=0; i<repeats; i++) {
+        // Create a vector with random integers (seeded)
+        std::srand(42);
+        std::generate(arr.begin(), arr.end(), std::rand);
+
+        // Sort array in place (only time the sort)
+        t1 = std::chrono::steady_clock::now();
+        merge_sort(arr, 0, array_size-1);
+        t2 = std::chrono::steady_clock::now();
+
+        // Accumulate measurement time
+        dt += std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();
+
+        // Check the sort actually worked
+        if (!verify_sorted(arr, array_size)) {
+            std::cerr << "Merge sort failure!" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        // Use values in the sorted array to prevent the compiler optimising away ops
+        dummy_val += arr[i % array_size];
+    }
+
+    std::cout << "Merge sort: " << ((float)dt / (1e6 * repeats)) << " s (average per op)" <<std::endl;
 
 
     // Dump final accumulated value to prevent compiler optimising away ops
